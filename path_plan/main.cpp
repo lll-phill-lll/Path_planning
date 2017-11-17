5307e9cd25a70e1e97a49748dfc25e32a1174536
lll-phill-lll <mefilitov@edu.hse.ru>
2017-11-17 16:29:34 +0300

    Try_again2

--- path_plan/main.cpp
+++ path_plan/main.cpp
@@ -5,7 +5,7 @@
 
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World2!" << std::endl;
     TiXmlDocument doc( "example.xml" );
     doc.LoadFile();
     TiXmlElement * root = doc.FirstChildElement();
